143be59255757029d5cc364d0177102253f65300
Dave Walton <dw-git@digger.net>
2017-04-27 16:58:40 -0700

    Add license.

--- src/net/digger/util/Delay.java
+++ src/net/digger/util/Delay.java
@@ -2,6 +2,28 @@ package net.digger.util;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * net.digger.util.Delay
+ * Copyright © 2017  David Walton
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Utility for pausing a program briefly.
+ * @author walton
+ */
 public class Delay {
 	public static final int NANO_PRECISION = setNanoPrecision();
 
